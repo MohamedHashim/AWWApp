4c1bff9665319f384c8e08dda5d712391d92adea
MohamedHashim <sawmido2000@gmail.com>
2017-04-13 16:55:01 +0200

    Initial commit

---
+++ app/src/main/java/com/mohamedhashim/awwapp/MainActivity.java
@@ -0,0 +1,166 @@
+package com.mohamedhashim.awwapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.InterstitialAd;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AdView mAdView;
+    private String TAG = MainActivity.class.getSimpleName();
+    InterstitialAd mInterstitialAd;
+    private FloatingActionButton share;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        Typeface typeFace = Typeface.createFromAsset(getAssets(), "fonts/White Chocolate Mint - TTF.ttf");
+
+//        app_name = (TextView) findViewById(R.id.app_name);
+//        app_name.setTypeface(typeFace);
+        share = (FloatingActionButton) findViewById(R.id.share);
+        share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    Intent i = new Intent(Intent.ACTION_SEND);
+                    i.setType("text/plain");
+                    i.putExtra(Intent.EXTRA_SUBJECT, "AutoDraw");
+                    String sAux = "\nLet me recommend you the AutoDraw App\n\n";
+                    sAux = sAux + "https://play.google.com/store/apps/details?id=com.mohamedhashim.awwapp \n\n";
+                    i.putExtra(Intent.EXTRA_TEXT, sAux);
+                    startActivity(Intent.createChooser(i, "choose one"));
+                } catch (Exception e) {
+                    //e.toString();
+                }
+            }
+        });
+        mInterstitialAd = new InterstitialAd(this);
+
+        // set the ad unit ID
+        mInterstitialAd.setAdUnitId(getString(R.string.interstitial));
+
+        AdRequest adRequest = new AdRequest.Builder()
+                .build();
+
+        // Load ads into Interstitial Ads
+        mInterstitialAd.loadAd(adRequest);
+
+        mInterstitialAd.setAdListener(new AdListener() {
+            public void onAdLoaded() {
+                showInterstitial();
+            }
+        });
+
+
+        mAdView = (AdView) findViewById(R.id.adView);
+        AdRequest adRequest2 = new AdRequest.Builder()
+                .build();
+        mAdView.loadAd(adRequest2);
+        mAdView.setAdListener(new AdListener() {
+            @Override
+            public void onAdLoaded() {
+//                Toast.makeText(getApplicationContext(), "Ad is loaded!", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onAdClosed() {
+//                Toast.makeText(getApplicationContext(), "Ad is closed!", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onAdFailedToLoad(int errorCode) {
+//                Toast.makeText(getApplicationContext(), "Ad failed to load! error code: " + errorCode, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onAdLeftApplication() {
+//                Toast.makeText(getApplicationContext(), "Ad left application!", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onAdOpened() {
+//                Toast.makeText(getApplicationContext(), "Ad is opened!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        WebView browser = (WebView) findViewById(R.id.webview);
+
+        if (isConnected()) {
+            browser.setWebViewClient(new MyBrowser());
+            browser.getSettings().setLoadsImagesAutomatically(true);
+            browser.getSettings().setJavaScriptEnabled(true);
+            browser.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+            browser.loadUrl("https://awwapp.com/");
+//            if(browser.islo)
+        } else {
+            Toast.makeText(getApplicationContext(), "Check your internet connection", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    public void onPause() {
+        if (mAdView != null) {
+            mAdView.pause();
+        }
+        super.onPause();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (mAdView != null) {
+            mAdView.resume();
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        if (mAdView != null) {
+            mAdView.destroy();
+        }
+        super.onDestroy();
+    }
+
+    private void showInterstitial() {
+        if (mInterstitialAd.isLoaded()) {
+            mInterstitialAd.show();
+        }
+    }
+
+    private class MyBrowser extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+    }
+
+    public boolean isConnected() {
+        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Activity.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected())
+            return true;
+        else
+            return false;
+    }
+
+}
